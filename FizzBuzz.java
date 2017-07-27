c424a210314bf096e134ee38dee9cb02147e9876
moosecanswim <kyle.gallagher.09@gmail.com>
2017-07-27 11:25:49 -0400

    modified it to simplify process and add codewords for multiples of 2 and 10

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -2,22 +2,39 @@ package fizzBuzz;
 
 public class FizzBuzz {
 	public static void main(String[] args){
-		for(int i = 0; i <= 100; i ++){
+		/**
+		*This is an application that displays "Fizz" for multipliers of 3, "Buzz" for 5, "Copy" for 2" and "cat" for 10
+		**/
+		int i = 1;
+		String output=null;
+		do{
+			boolean printNumber = true;
+			output="";
+			if(i%2==0){
+				output+="Copy";
+				printNumber = false;
+			}
+			if(i%3==0){
+				output+="Fizz";
+				printNumber = false;
+			}
+			if(i%5==0){
+				output+="Buzz";
+				printNumber = false;
+			}
+			if(i%10==0){
+				output+="Cat";
+				printNumber = false;
+			}
+			if(printNumber == true){
+				output = String.valueOf(i);
+			}
+			System.out.println(output);
+			
 
-			output(i);
-		}
+			i++;
+		}while(i < 101);
 	}
 
-	public static void output (int input){
-		if(input%3 ==0 && input%5 ==0){
-			System.out.println("fizzbuzz");
-		}else if(input%3==0){
-			System.out.println("fizz");
-		}
-		else if(input%5==0){
-			System.out.println("buzz");
-		}else{
-			System.out.println(input);
-		}
-	}
+
 }
